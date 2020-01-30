bec94b62f57bb72cf38484daa3243aff582a939f
ToniE <osm-ToniE@web.de>
2020-01-30 10:56:57 +0100

    translate into english (issue #3)

--- en/config.php
+++ en/config.php
@@ -37,13 +37,47 @@
             </div>
         </header>
 
+        <nav id="navigation">
+            <h2 id="de">Configuration details <?php if ( $found ) { printf( "fpr %s", $_GET['network'] ); } ?></h2>
+            <ul>
+                <li><a href="#overpass-api">Overpass-API Query</a></li>
+                <li><a href="#options">Analysis options</a></li>
+             </ul>
+        </nav>
+
+        <hr />
+
         <main id="main" class="results">
+
+            <h2 id="overpass-api">Overpass-API Query</h2>
+            <p>
+                The <a href="https://wiki.openstreetmap.org/wiki/Overpass_API">Overpass API</a> is used to download the OSM data.
+                <a href="/en/index.php#overpass">The query used</a> returns all ways and nodes of the routes (their members with their details) from a defined search area.
+                The data thus obtained allow an analysis of the public transport lines to the effect that e.g. the route can also be checked for completeness.
+                Nodes, ways and relations (stops and platforms) and their tags can be checked against their 'role' in the relation.
+            </p>
+            
+            <?php if ( $found ) { 
+                      printf( "<p><code>%s</code></p>\n", htmlentities(GetOverpassQuery()) );
+                      printf( "<p>This query currently delivers approximately %.1f MB.\n</p>", GetOsmXmlFileSize() );
+                      printf( "<p>Show the search area on the <a href=\"%s\">OSM map</a>.</p>\n", GetRegionLink() );
+                  }
+            ?>
+
+            <hr />
+
+            <h2 id="options">Analysis options</h2>
+
+            <p>
+                The output of errors and comments can be influenced by a variety of analysis options. <br />
+                Here is a <a href="/en/index.php#messages">list of the error messages and comments</a>.<br />
+            </p>
 
             <table id="message-table">
                 <thead>
                     <tr class="message-tableheaderrow">
                         <th class="message-text">Option</th>
-                        <th class="message-option">Wert</th>
+                        <th class="message-option">Value</th>
                     </tr>
                 </thead>
                 <tbody>
