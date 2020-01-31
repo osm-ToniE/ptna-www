d590a7aad4f08f1ffb864beab5e98391fde5bbaa
ToniE <osm-ToniE@web.de>
2020-01-31 09:56:57 +0100

    add contents, translate

--- es/config.php
+++ es/config.php
@@ -37,7 +37,44 @@
             </div>
         </header>
 
+        <nav id="navigation">
+            <h2 id="de">Configuration details <?php if ( $found ) { printf( "for %s", $_GET['network'] ); } ?></h2>
+            <ul>
+                <li><a href="#overpass-api">Consulta API Overpass</a></li>
+                <li><a href="#options">Opciones de análisis</a></li>
+             </ul>
+        </nav>
+
+        <hr />
+
         <main id="main" class="results">
+
+            <h2 id="overpass-api">Consulta API Overpass</h2>
+            <p>
+                La <a href="https://wiki.openstreetmap.org/wiki/Overpass_API">Overpass API</a> se usa para descargar los datos de OSM.
+                <a href="/en/index.php#overpass">La consulta utilizada</a> devuelve todas las formas y nodos de las rutas (sus miembros con sus detalles) desde un <a href="/en/index.php#searcharea"> área de búsqueda</a>.
+                Los datos así obtenidos permiten un análisis de las líneas de transporte público en el sentido de que, p. la ruta también se puede verificar para completar.
+                Los nodos, formas y relaciones (paradas y plataformas) y sus etiquetas pueden verificarse con respecto a su 'rol' en la relación.
+            </p>
+            
+            <?php if ( $found ) {
+                      $query = htmlentities( GetOverpassQuery() );
+                      $fsize = GetOsmXmlFileSize();
+                      $rlink = GetRegionLink();
+                      if ( $query ) { printf( "<p><code>%s</code></p>\n", $query ); }
+                      if ( $fsize ) { printf( "<p>Esta consulta actualmente ofrece aproximadamente %.1f MB.\n</p>", $fsize ); }
+                      if ( $rlink ) { printf( "<p>Mostrar el área de búsqueda en el <a href=\"%s\">OSM map</a>.</p>\n", $rlink ); }
+                  }
+            ?>
+
+            <hr />
+
+            <h2 id="options">Opciones de análisis</h2>
+
+            <p>
+                La salida de <a href="/en/index.php#messages">errores y comentarios</a> puede controlarse mediante una variedad de <a href="/en/index.php#options">opciones de análisis< / a>. <br />
+                Aquí hay una lista de opciones de análisis y sus valores.<br />
+            </p>
 
             <table id="message-table">
                 <thead>
