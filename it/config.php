df7010391e43c167026720027157dd7a2bc84774
ToniE <osm-ToniE@web.de>
2020-01-31 10:09:39 +0100

    add contents and translate

--- it/config.php
+++ it/config.php
@@ -37,7 +37,44 @@
             </div>
         </header>
 
+        <nav id="navigation">
+            <h2 id="de">Configuration details <?php if ( $found ) { printf( "for %s", $_GET['network'] ); } ?></h2>
+            <ul>
+                <li><a href="#overpass-api">Query API overpass</a></li>
+                <li><a href="#options">Opzioni di analisi</a></li>
+             </ul>
+        </nav>
+
+        <hr />
+
         <main id="main" class="results">
+
+            <h2 id="overpass-api">Query API overpass</h2>
+            <p>
+                La <a href="https://wiki.openstreetmap.org/wiki/Overpass_API">API Overpass</a> viene utilizzata per scaricare i dati OSM.
+                <a href="/en/index.php#overpass">La query utilizzata</a> restituisce tutti i modi e nodi dei percorsi (i loro membri con i loro dettagli) da un <a href="/en/index.php#searcharea">area di ricerca</a>.
+                I dati così ottenuti consentono un'analisi delle linee di trasporto pubblico secondo cui ad es. il percorso può anche essere verificato per completezza.
+                Nodi, modi e relazioni (fermate e piattaforme) e i loro tag possono essere verificati rispetto al loro "ruolo" nella relazione.
+            </p>
+            
+            <?php if ( $found ) {
+                      $query = htmlentities( GetOverpassQuery() );
+                      $fsize = GetOsmXmlFileSize();
+                      $rlink = GetRegionLink();
+                      if ( $query ) { printf( "<p><code>%s</code></p>\n", $query ); }
+                      if ( $fsize ) { printf( "<p>Questa query attualmente recapita approssimativamente %.1f MB.\n</p>", $fsize ); }
+                      if ( $rlink ) { printf( "<p>Mostra l'area di ricerca sulla <a href=\"%s\">mappa OSM</a>.</p>\n", $rlink ); }
+                  }
+            ?>
+
+            <hr />
+
+            <h2 id="options">Opzioni di analisi</h2>
+
+            <p>
+                Gli <a href="/en/index.php#messages">errori e commenti</a> riportati da PTNA possono essere controllati da una varietà di <a href="/en/index.php#options">opzioni di analisi</a>.<br />
+                Ecco un elenco di opzioni di analisi e i loro valori.<br />
+            </p>
 
             <table id="message-table">
                 <thead>
