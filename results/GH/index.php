8e2a8143ecb3983a3f3664d5f904b5219687ee76
ToniE <osm-ToniE@web.de>
2020-03-12 11:13:21 +0100

    add index.php for Ghana

---
+++ results/GH/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title="Results"; $inc_lang='../../en/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+      
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="GH"><img src="/img/Ghana32.png" alt="flag of Ghana" /> Results for Ghana</h2>
+      
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksGhana">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry( "GH-AA-Accra", "en", "Configuration" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
+
