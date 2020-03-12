eb569198c9bad98c32ce5cede18ec50c82b2d687
ToniE <osm-ToniE@web.de>
2020-03-12 11:15:14 +0100

    add index.php for Ethiopia

---
+++ results/ET/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title="Results"; $inc_lang='../../en/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+      
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="ET"><img src="/img/Ethiopia32.png" alt="flag of Ethiopia" /> Results for Ethiopia</h2>
+      
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksEthiopia">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry( "ET-AA-Addis-Ababa", "en", "Configuration" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
+
