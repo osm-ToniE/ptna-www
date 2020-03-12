0b81d6d2127cad22cd50b98ddfd65fc839a1abf9
ToniE <osm-ToniE@web.de>
2020-03-12 10:48:11 +0100

    add index.php for Bolivia

---
+++ results/BO/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php $title="Resultados"; $inc_lang='../../es/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+      
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="BO"><img src="/img/Bolivia32.png" alt="bandera Bolivia" /> Resultados para Bolivia</h2>
+      
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksBO">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry( "BO-C-Cochabamba", "es", "Configuración" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
+
