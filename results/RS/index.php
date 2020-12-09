f33d4c9481dce422822d2002935ad17267d41714
Toni Erdmann <Toni.Erdmann@web.de>
2020-12-09 09:41:13 +0100

    small fix, add blank

--- results/RS/index.php
+++ results/RS/index.php
@@ -12,7 +12,7 @@
 
         <main id="main" class="results">
 
-            <h2 id="RS"><img src="/img/Serbia32.png" alt="застава Србије" />Резултати за Србију</h2>
+            <h2 id="RS"><img src="/img/Serbia32.png" alt="застава Србије" /> Резултати за Србију</h2>
 
 <?php include $inc_lang.'results-head.inc' ?>
 
