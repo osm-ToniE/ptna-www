08d0ead4c5b9dc1ffacda9af594fc53a5412de49
Toni Erdmann <Toni.Erdmann@web.de>
2021-01-14 17:19:18 +0100

    support french language

--- fr/config.php
+++ fr/config.php
@@ -8,9 +8,9 @@
     <body>
 
 <?php if ( isset($_GET['network']) ) { $found = ReadDetails( $_GET['network'] ); } ?>
-        
+
       <div id="wrapper">
-      
+
 <?php include "header.inc" ?>
 
         <nav id="navigation">
@@ -35,12 +35,12 @@
             <div class="indent">
 
                 <p>
-                    <a href="https://wiki.openstreetmap.org/wiki/Overpass_API">L'API Overpass</a> est utilisée pour télécharger les données OSM.
-                    <a href="/en/index.php#overpass">La requête utilisée</a> renvoie tous les chemins et nœuds des itinéraires (leurs membres avec leurs détails) à partir d'une <a href = "/en/index.php#searcharea">zone de recherche</a>.
+                    <a href="https://wiki.openstreetmap.org/wiki/FR:Overpass_API">L'API Overpass</a> est utilisée pour télécharger les données OSM.
+                    <a href="/fr/index.php#overpass">La requête utilisée</a> renvoie tous les chemins et nœuds des itinéraires (leurs membres avec leurs détails) à partir d'une <a href = "/fr/index.php#searcharea">zone de recherche</a>.
                     Les données ainsi obtenues permettent une analyse des itinéraires de transports en commun pour par exemple vérifier l'intégralité de l'itinéraire.
                     Les nœuds, voies et relations (arrêts et plateformes) ainsi que leurs attributs peuvent être vérifiés ainsi leur «rôle» dans la relation.
                 </p>
-                
+
                 <?php if ( $found ) {
                           $query = htmlentities( GetOverpassQuery() );
                           $fsize = GetOsmXmlFileSizeByte();
@@ -48,8 +48,8 @@
                           $rname = htmlentities( GetRegionName() );
                           if ( $query ) { printf( "<p><code>%s</code></p>\n", $query ); }
                           if ( $fsize ) { printf( "<p>Cette requête fournit actuellement environ %.1f MB.\n</p>", $fsize / 1024 / 1024 ); }
-                          if ( $rlink ) { 
-                              printf( "<p>Afficher la <a href=\"/en/index.php#searcharea\">zone de recherche</a> " );
+                          if ( $rlink ) {
+                              printf( "<p>Afficher la <a href=\"/fr/index.php#searcharea\">zone de recherche</a> " );
                               if ( $rname ) { printf( "\"<strong>%s</strong>\" ", $rname ); }
                               printf( "sur la <a href=\"%s\">carte OSM</a>.</p>\n", $rlink );
                           }
@@ -63,10 +63,10 @@
             <div class="indent">
 
                 <p>
-                    Les <a href="/en/index.php#messages">erreurs et commentaires</a> signalés par PTNA peuvent être contrôlés par diverses <a href="/en/index.php#options">options d'analyse</a>.<br />
+                    Les <a href="/fr/index.php#messages">erreurs et commentaires</a> signalés par PTNA peuvent être contrôlés par diverses <a href="/fr/index.php#options">options d'analyse</a>.<br />
                     Voici une liste des options d'analyse et leurs valeurs.<br />
                 </p>
-    
+
                 <table id="message-table">
                     <thead>
                         <tr class="message-tableheaderrow">
@@ -75,7 +75,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php if ( $found ) { PrintOptionDetails( "en" ); } ?>
+                        <?php if ( $found ) { PrintOptionDetails( "fr" ); } ?>
                     </tbody>
                 </table>
             </div>
@@ -92,7 +92,7 @@
                 <div class="indent">
 
                     <p>
-                        <?php $link = GetDiscussionPagePtna(); 
+                        <?php $link = GetDiscussionPagePtna();
                               if ( $link ) {
                                   printf( "<a href=\"%s\">Discussion générale sur le PTNA</a> dans le Wiki de l'OSM.", $link );
                               } else {
@@ -106,7 +106,7 @@
                 <div class="indent">
 
                     <p>
-                        <?php $link = GetDiscussionPageNetwork(); 
+                        <?php $link = GetDiscussionPageNetwork();
                               if ( $link ) {
                                   printf( "<a href=\"%s\">Discussion pour cette analyse</a>, ce 'network' dans le Wiki de l'OSM.", $link );
                               } else {
@@ -126,4 +126,3 @@
       </div> <!-- wrapper -->
     </body>
 </html>
-
