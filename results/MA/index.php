0b74524022ee32dc864ad5442d7ab36c65794b2f
Toni Erdmann <Toni.Erdmann@web.de>
2021-02-13 19:51:22 +0100

    add city Tetouan of Morocco

---
+++ results/MA/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php $title="Résultats"; $inc_lang='../../fr/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="MA"><img src="/img/Morocco32.png" alt="Drapeau marocain" /> Résultats pour Maroc</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksMA">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry("MA-01-Tetouan", "fr", "Configuration" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
