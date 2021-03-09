4656af245bafc1ca0e60197fdb47b41d1f3448d1
Toni Erdmann <Toni.Erdmann@web.de>
2021-03-09 11:15:27 +0100

    add public transpoert for MR-NKC-Nouakchott

---
+++ results/MR/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php $title="Résultats"; $inc_lang='../../fr/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="MR"><img src="/img/Mauritania32.png" alt="Drapeau mauritanien" /> Résultats pour Mauritanie</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksMR">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry("MR-NKC-Nouakchott", "fr", "Configuration" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
