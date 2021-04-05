2d40212ee1c3929f5f18bee2a8f98b2617940fce
Toni Erdmann <Toni.Erdmann@web.de>
2021-04-05 12:02:15 +0200

    add Graz's part of VVSt in Austria

---
+++ results/AT/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<?php $title="Auswertungen"; $inc_lang='../../de/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+<main id="main" class="results">
+
+<h2 id="AT"><img src="/img/Austria32.png" alt="Fagge Österreich" /> Auswerungen für Österreich</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+<table id="networksAT">
+    <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+    </thead>
+    <tbody>
+
+        <?php CreateNewFullEntry( "AT-6-VVSt-Graz", "de", "Konfiguration" ); ?>
+
+    </tbody>
+</table>
+
+</main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
