778de1780d0d3c8d82fd0ec292eba24e2d0f92c8
Toni Erdmann <Toni.Erdmann@web.de>
2021-11-06 13:40:49 +0100

    fix bug in analysis of "$osm['gtfs_short_name_hack1']"
    - introduced when replacing
      - if $osm['gtfs_short_name_hack1']  by
      - if isset($osm['gtfs_short_name_hack1'])

--- script/gtfs.php
+++ script/gtfs.php
@@ -1092,11 +1092,12 @@
                         $osm_route          = htmlspecialchars(RouteType2OsmRoute($routes['route_type']));
                         $osm_vehicle        = OsmRoute2Vehicle($osm_route,$ptna['language']);
                         $osm_ref            = $routes['route_short_name']       ? htmlspecialchars($routes['route_short_name'])     : '???';
-                        if ( isset($osm['gtfs_short_name_hack1'])              &&
-                            $routes['route_long_name']                         &&
-                            $routes['route_id']                                &&
-                            $routes['route_long_name'] != $routes['route_id']      ) {
-                            $osm_ref = htmlspecialchars( $routes['route_long_name'] );
+                        if ( isset($osm['gtfs_short_name_hack1'])               &&
+                             $osm['gtfs_short_name_hack1']                      &&
+                             $routes['route_long_name']                         &&
+                             $routes['route_id']                                &&
+                             $routes['route_long_name'] != $routes['route_id']      ) {
+                             $osm_ref = htmlspecialchars( $routes['route_long_name'] );
                         }
                         if ( preg_match("/$osm_vehicle$/",$osm_ref) ) {
                             $osm_ref = preg_replace( "/\s+$osm_vehicle$/", "", $osm_ref );
