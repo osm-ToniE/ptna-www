406e998f6a1faa6e695501429773ac20b2ac673b
Toni Erdmann <Toni.Erdmann@web.de>
2022-02-25 14:18:05 +0100

    add analysis for Argentina AMBA

---
+++ results/AR/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php $title="Resultados"; $inc_lang='../../es/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="CO"><img src="/img/Argentina32.png" alt="bandera Argentina" /> Resultados para Argentina</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksAR">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry( "AR-AMBA", "es", "Configuración" ); ?>
+
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
