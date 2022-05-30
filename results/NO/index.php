1debab37e7a5de4fc0e7f4b9f0c00430ad928ef5
Toni Erdmann <Toni.Erdmann@web.de>
2022-05-30 13:26:54 +0200

    prepare for PTNA analysis for Norway

---
+++ results/NO/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="no">
+
+<?php $title="Results"; $inc_lang='../../en/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="NO"><img src="/img/Norway32.png" alt="norks flagg" /> Resultater for Norge</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksNO">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <!--- <?php CreateNewFullEntry( "NO-", "no", "Configuration" ); ?> --->
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
