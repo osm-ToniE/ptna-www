ca4b3c2c016d28d819e13ce814b2e329a80b205c
Toni Erdmann <Toni.Erdmann@web.de>
2023-01-07 15:39:02 +0100

    add GTFS-feed FR-PAC-Orizo

--- gtfs/FR/index.php
+++ gtfs/FR/index.php
@@ -22,6 +22,7 @@
                     </thead>
                     <tbody>
 <?php
+    $duration += CreateGtfsEntry( "FR-PAC-Orizo" );
     $duration += CreateGtfsEntry( "FR-PAC-Zou-Express" );
     $duration += CreateGtfsEntry( "FR-PAC-Zou-Proximite" );
     $duration += CreateGtfsEntry( "FR-PAC-Zou-Scolaire" );
