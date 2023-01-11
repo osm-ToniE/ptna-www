03f2f91e994b7eb2808d02897c232722e368132d
Toni Erdmann <Toni.Erdmann@web.de>
2023-01-11 20:13:27 +0100

    add network SI-Ljubljana-MPP in Slovenia

---
+++ results/SI/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="sl">
+
+<?php $title="Results"; $inc_lang='../../en/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="SI"><img src="/img/Slovenia32.png" alt="zastava slovenije" /> Rezultati za Slowenia</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksSI">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                <?php CreateNewFullEntry( "SI-Ljubljana-MPP", "en", "Konfiguracija" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
