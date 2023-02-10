6c43b79316005ee5e5701453c6356f4b2d4f7fe8
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-10 12:46:42 +0100

    fix issue #44
    sort only if number of different durations == 1

--- script/gtfs.php
+++ script/gtfs.php
@@ -1873,7 +1873,9 @@
                                     $departures  = preg_replace( "/(\d{1,2}:\d\d):\d\d,/", "\\1,", $service_departure[$row["service_id"]] );
                                     $departures  = preg_replace( "/,$/", "", $departures );
                                     $unique_departures = array_flip( array_flip( explode( ',', $departures ) ) );
-                                    sort( $unique_departures );
+                                    if ( count($different_durations) == 1 ) {
+                                        sort( $unique_departures );
+                                    }
                                     $service_row .= htmlspecialchars( implode( ', ', $unique_departures ) );
                                     $service_row .= "</td>\n                                ";
                                     $service_row .= '<td class="gtfs-text"' . $style_width_durations . '>';
