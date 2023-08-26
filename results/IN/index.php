fd27872800a0b35adfbde0e3d75265944794ff94
Toni Erdmann <Toni.Erdmann@web.de>
2023-08-26 15:09:07 +0200

    prepare for more analysis results in India

--- results/IN/index.php
+++ results/IN/index.php
@@ -12,7 +12,7 @@
 
         <main id="main" class="results">
 
-            <h2 id="AU"><img src="/img/India32.png" alt="Flag of India" /> Results for India</h2>
+            <h2 id="IN"><img src="/img/India32.png" alt="Flag of India" /> Results for India</h2>
 
 <?php include $inc_lang.'results-head.inc' ?>
 
@@ -22,7 +22,35 @@
                 </thead>
                 <tbody>
 
+                    <?php CreateNewFullEntry( "IN-DL-Delhi", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-GJ-Ahmedabad", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-HR-Gurgaon", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-KA-Bengaluru", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-KL-Kochi", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-MH-Mumbai", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-MH-Pune", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-RJ-Jaipur", "en", "Configuration" ); ?>
+
                     <?php CreateNewFullEntry( "IN-TN-Chennai", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-TS-Hyderabad", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-UP-Kanpur", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-UP-Lucknow", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-UP-Nagpur", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-UP-Noida", "en", "Configuration" ); ?>
+
+                    <?php CreateNewFullEntry( "IN-WB-Kolkata", "en", "Configuration" ); ?>
 
                 </tbody>
             </table>
