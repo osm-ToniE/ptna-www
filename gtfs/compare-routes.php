9b84bdf6af0a95ecf5bd1b8794270ddcfce01e9c
Toni Erdmann <Toni.Erdmann@web.de>
2024-02-20 13:58:11 +0100

    add manually compiled data

--- gtfs/compare-routes.php
+++ gtfs/compare-routes.php
@@ -27,25 +27,35 @@
 
         <main id="main" class="results">
 
-<?php   if ( $prove_of_concept ): ?>
             <h2 id="compare-routes">Compare GTFS route with OSM route_master</h2>
             <div class="indent">
                 <p>
-                    <span style="background-color: orange; font-weight: 1000; font-size:2.0em;">This is proof-of-concept (fake) data based on a specific bus: DE-BY-MVV Bus 210. Just to discuss the layout of this page, ...</span>
+                    <span style="background-color: orange; font-weight: 1000; font-size:2.0em;">This is proof-of-concept (manually compiled) data based on a specific bus: DE-BY-MVV Bus 210.<br/>Just to discuss the layout of this page, ...</span>
                 </p>
 
                 <?php CreateCompareRoutesTable( $feed, $feed2, $release_date, $release_date2, $route_id, $route_id2, $osm_relation, $ptna_lang ); ?>
 
-                Small values indicate a good match between GTFS trip and OSM route.
-                    <ul>
-                        <li>xS == number of stops differs by "x"</li>
-                        <li>yP == number of stops where positions differ by more than 20 meters (~ maximum length of a bus)</li>
-                    </ul>
-                For a more detailed comparison, click on a number.
+                Small values indicate a good match between GTFS trip and OSM route.<br/>
+                <p>For a more detailed comparison, click on a number.</p>
+                <p>Colours are calculated as follows:</p>
+                <ul>
+                <li><span style="background-color: #6aef00;">0 <= score < 2</span></li>
+                <li><span style="background-color: #aecd00;">2 <= score < 12</span></li>
+                <li><span style="background-color: #d7a700;">12 <= score < 24</span></li>
+                <li><span style="background-color: #f17a00;">24 <= score < 48</span></li>
+                <li><span style="background-color: #fe4000;">48 <= score</span></li>
+                </ul>
+                <p>Move the mouse over a cell and it will pop-up a list of individual scores (yet to be done).</p>
+                <ul>
+                    <li>xS == number of stops differ by "x%"</li>
+                    <li>(a,b,c)P == percentage of stops where positions differ by more than 20 / 100 / 1000 meters</li>
+                    <li>xN == percentage of stops where the 'name' differs (GTFS-'stop_name' / OSM-'name')</li>
+                    <li>xR == percentage of stops where the GTFS-'Stop_name' fiffers from OSM-'ref_name' (if tagged)</li>
+                    <li>xI == percentage of stops where the GTFS-'stop_id' differ (GTFS/GTFS comparison)</li>
+                    <li>xG == percentage of stops where the GTFS-'stop_id' differs from OSM-'gtfs:stop_id' (if tagged)</li>
+                    <li>xF == percentage of stops where the GTFS-'stop_id' differs from OSM-'ref:IFOPT' (if tagged)</li>
+                </ul>
             </div>
-<?php   else: ?>
-            <h2 id="compare-routes">Compare GTFS route with GTFS route</h2>
-<?php   endif ?>
 
         </main> <!-- main -->
 
