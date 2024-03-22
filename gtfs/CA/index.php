3f5e4b97db51e8283a118da68c8ee242e1b5eafb
Toni Erdmann <Toni.Erdmann@web.de>
2024-03-22 14:37:22 +0100

    add GTFS feed for CE-QC-RTC

--- gtfs/CA/index.php
+++ gtfs/CA/index.php
@@ -23,6 +23,7 @@
                     <tbody>
 <?php
     $duration += CreateGtfsEntry( "CA-MB-WT" );
+    $duration += CreateGtfsEntry( "CA-QC-RTC" );
 ?>
                     </tbody>
                 </table>
