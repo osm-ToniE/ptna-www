298a9b2940559d29d713381d0bf8c703f55b3160
Toni Erdmann <Toni.Erdmann@web.de>
2024-03-31 14:24:37 +0200

    add support for BG-22-Sofia

---
+++ results/BG/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title="Results"; $inc_lang='../../en/'; include $inc_lang.'html-head.inc'; ?>
+
+<?php include('../../script/entries.php'); ?>
+
+    <body>
+      <div id="wrapper">
+
+<?php include $inc_lang.'header.inc' ?>
+
+        <main id="main" class="results">
+
+            <h2 id="BG"><img src="/img/Bulgaria32.png" class="flagimg" alt="българско знаме" /> Results for България / Bulgaria</h2>
+
+<?php include $inc_lang.'results-head.inc' ?>
+
+            <table id="networksBG">
+                <thead>
+<?php include $inc_lang.'results-trth.inc' ?>
+                </thead>
+                <tbody>
+
+                    <?php CreateNewFullEntry( "BG-22-Sofia", "bg", "Конфигурация" ); ?>
+
+                </tbody>
+            </table>
+
+        </main> <!-- main -->
+
+        <hr />
+
+<?php include $inc_lang.'footer.inc' ?>
+
+      </div> <!-- wrapper -->
+    </body>
+</html>
