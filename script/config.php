0a9518f63df2b7559e82ab76ae2b7fd1298e38d2
Toni Erdmann <Toni.Erdmann@web.de>
2024-04-27 17:25:17 +0200

    add support for new option "--check-against-gtfs"

--- script/config.php
+++ script/config.php
@@ -9,6 +9,7 @@
 
         $options_hash['allow-coach']                = 'OFF';
         $options_hash['check-access']               = 'OFF';
+        $options_hash['check-against-gtfs']         = 'OFF';
         $options_hash['check-bus-stop']             = 'OFF';
         $options_hash['check-gtfs']                 = 'OFF';
         $options_hash['check-motorway-link']        = 'OFF';
