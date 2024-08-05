76df861f04274ebcfc2288f6226a0eb41fb89f7f
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-05 15:13:55 +0200

    do not show "pure planet update" tasks, only those with 'filter' and 'extract'

--- en/statistics.php
+++ en/statistics.php
@@ -30,19 +30,8 @@
 
             <hr />
 
-            Planet update and extract:
+            Planet update, filter, and extracts:
             <a href="showlogs.php?planet=UTC%2B01" title="Show logs for planet handling: Africa + Europe">Africa and Europe @ 01:15</a>
-
-            <hr />
-
-            Planet update:
-            <a href="showlogs.php?planet=0415" title="Show logs for planet handling">04:15</a>
-            <a href="showlogs.php?planet=0715" title="Show logs for planet handling">07:15</a>
-            <a href="showlogs.php?planet=1015" title="Show logs for planet handling">10:15</a>
-            <a href="showlogs.php?planet=1315" title="Show logs for planet handling">13:15</a>
-            <a href="showlogs.php?planet=1615" title="Show logs for planet handling">16:15</a>
-            <a href="showlogs.php?planet=1915" title="Show logs for planet handling">19:15</a>
-            <a href="showlogs.php?planet=2215" title="Show logs for planet handling">22:15</a>
 
             <hr />
 
