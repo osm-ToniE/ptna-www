dc9f52360478ce0053ab77213eb2879770f91e68
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-06 13:06:15 +0200

    add GTFS data provided by "Transport for New South Wales", Australia

--- gtfs/AU/index.php
+++ gtfs/AU/index.php
@@ -22,6 +22,7 @@
                     </thead>
                     <tbody>
 <?php
+    $duration += CreateGtfsEntry( "AU-NSW-TfNSW" );
     $duration += CreateGtfsEntry( "AU-QD-South-East-Queensland" );
     $duration += CreateGtfsEntry( "AU-SA-Adelaide-Metro" );
 ?>
