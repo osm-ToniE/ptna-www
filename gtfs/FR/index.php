e189f3821d79fa830c426d6b529e3b92f8b3ee4b
Toni Erdmann <Toni.Erdmann@web.de>
2025-02-07 17:27:20 +0100

    fix copy&paste error

--- gtfs/FR/index.php
+++ gtfs/FR/index.php
@@ -46,7 +46,7 @@
     $duration += CreateGtfsEntry( "FR-OCC-Tisseo" );
     $duration += CreateGtfsEntry( "FR-PAC-Altigo" );
     $duration += CreateGtfsEntry( "FR-PAC-Bandol-et-Sanary-sur-mer" );
-    $duration += CreateGtfsEntry( "FR-PAC-FR-PAC-Cmonbus" );
+    $duration += CreateGtfsEntry( "FR-PAC-Cmonbus" );
     $duration += CreateGtfsEntry( "FR-PAC-L-Agglo-en-bus" );
     $duration += CreateGtfsEntry( "FR-PAC-Lignes-d-Azur" );
     $duration += CreateGtfsEntry( "FR-PAC-Mistral" );
