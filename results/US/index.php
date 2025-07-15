d5305c847e2794a87ae41055747018eda30183cf
Toni Erdmann <Toni.Erdmann@web.de>
2025-07-15 13:23:50 +0200

    add US-CA-LACMTA network

--- results/US/index.php
+++ results/US/index.php
@@ -25,6 +25,7 @@
                 <?php CreateNewFullEntry( "US-Amtrak", "en", "Configuration" ); ?>
                 <?php CreateNewFullEntry( "US-Flixbus", "en", "Configuration" ); ?>
                 <?php CreateNewFullEntry( "US-AK-Anchorage-PTD", "en", "Configuration" ); ?>
+                <?php CreateNewFullEntry( "US-CA-LACMTA", "en", "Configuration" ); ?>
                 <?php CreateNewFullEntry( "US-CA-SantaCruz", "en", "Configuration" ); ?>
                 <?php CreateNewFullEntry( "US-DC-WMATA", "en", "Configuration" ); ?>
                 <?php CreateNewFullEntry( "US-IL-Metra", "en", "Configuration" ); ?>
