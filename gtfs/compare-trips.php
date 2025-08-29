501af9dae8ea33fc46a4aff127e8b5fc05152a7d
Toni Erdmann <Toni.Erdmann@web.de>
2025-08-30 00:17:25 +0200

    download also LeafLet plugin polylineDecorator

--- gtfs/compare-trips.php
+++ gtfs/compare-trips.php
@@ -22,7 +22,7 @@
     <body>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/leaflet-polylinedecorator@1.6.0/dist/leaflet.polylineDecorator.min.js"></script>
         <script src="/script/gtfs-compare.js"></script>
 
         <div id="wrapper">
